32d48fe91ceef9525d590948468f6ec7487403f5
Hedgehog <hedgehogshiatus@gmail.com>
2011-03-14 22:49:04 +1100

    Add Cucumber/Aruba friendly options available.
    Signed-off-by: Hedgehog <hedgehogshiatus@gmail.com>

--- lib/ssh-forever.rb
+++ lib/ssh-forever.rb
@@ -14,12 +14,8 @@ module SshForever
 
     def run
       unless File.exists?(public_key_path)
-        STDERR.puts "You do not appear to have a public key. I expected to find one at #{public_key_path}\n"
-        STDERR.print "Would you like me to generate one? [Y/n]"
-        result = STDIN.gets.strip
-        unless result == '' or result == 'y' or result == 'Y'
-          flunk %Q{Fair enough, I'll be off then. You can generate your own by hand using\n\n\tssh-keygen -t rsa}
-        end
+        STDERR.puts "You do not appear to have a public key. I expected to find one at #{public_key_path}"  unless @options[:quiet]
+        confirm_keygen
         generate_public_key
       end
 
@@ -27,7 +23,7 @@ module SshForever
 
       copy_public_key(args)
 
-      puts "Success. From now on you can just use plain old 'ssh'. Logging you in..."
+      puts "Success. From now on you can just use plain old 'ssh'. Logging you in..."  unless @options[:quiet]
       status = run_shell_cmd(ssh_login(args))
       exit 1 unless status.exitstatus.to_i == 0
     end
@@ -43,10 +39,17 @@ module SshForever
     end
 
     def append_ssh_config
-      puts "Creating host entry in local ssh config with name #{@options[:name]}"
+      puts "Creating host entry in local ssh config with name #{@options[:name]}" unless @options[:quiet]
       (@local_ssh_config_path + '.ssh' + 'config').open("a") do |config|
         config << ssh_config
       end
+    end
+
+    def existing_ssh_config?
+      config = (@local_ssh_config_path + '.ssh' + 'config').read
+      chk1 = config[/IdentityFile #{public_key_path}/] ? true : false
+      chk2 = config[/Host #{@options[:name]}/] ? true : false
+      chk1 && chk2 ? true : false
     end
 
     def ssh_config
@@ -67,11 +70,22 @@ module SshForever
       status = Open4::popen4('sh') do |pid, stdin, stdout, stderr|
         stdin.puts cmd
         stdin.close
-        puts "stdout     : #{ stdout.read.strip }" if stdout.read.strip.length>0
-        puts "stderr     : #{ stderr.read.strip }" if stderr.read.strip.length>0
+        puts "stdout     : #{ stdout.read.strip }" if stdout.read.strip.length>0 and !@options[:quiet]
+        puts "stderr     : #{ stderr.read.strip }" if stderr.read.strip.length>0 and !@options[:quiet]
       end
       status
     end
+
+    def confirm_keygen
+      unless @options[:auto]
+        STDERR.print "Would you like me to generate one? [Y/n]"  unless @options[:quiet]
+        result = STDIN.gets.strip
+        unless result == '' or result == 'y' or result == 'Y'
+          flunk %Q{Fair enough, I'll be off then. You can generate your own by hand using\n\n\tssh-keygen -t rsa}
+        end
+      end
+    end
+
 
     def generate_public_key
       status = run_shell_cmd(ssh_keygen)
@@ -79,7 +93,8 @@ module SshForever
     end
 
     def copy_public_key(args)
-      puts "Copying your public key to the remote server. Prepare to enter your password for the last time."
+      puts "Copying your public key to the remote server."  unless @options[:quiet]
+      puts "Prepare to enter your password for the last time..." unless @options[:quiet]
       status = run_shell_cmd(ssh_keycopy(args))
       exit 1 unless status.exitstatus.to_i == 0
     end
@@ -102,11 +117,12 @@ module SshForever
 
     def ssh_login(args)
       if @options[:name]
-        append_ssh_config()
+        append_ssh_config unless existing_ssh_config?
         login_command = "ssh #{@options[:name]}#{args}"
       else
         login_command = "ssh #{@login}#{args}"
       end
+      login_command
     end
 
 
